b26ad98e912ffdd6a2ab72fc91715711ed560329
Avinash Gupta <avinash5693@gmail.com>
2025-01-17 00:59:33 +0530

    Button Animation has been added on the button from bottom to the top on the home page.

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.scss";
 import { useNavigate } from "react-router-dom";
+import { Animate } from "react-simple-animate";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -16,9 +17,20 @@ const Home = () => {
           Front end developer
         </h1>
       </div>
-      <div className="home__contact-me">
-        <button onClick={handleNavigationToContactMePage}> Hire Me</button>
-      </div>
+      <Animate
+      play
+      duration={1.5}
+      delay={1}
+      start={{
+      transform: "translateY(550px)"
+      }}
+      end={{
+      transform: "translatex(0px)"
+      }}>
+        <div className="home__contact-me">
+          <button onClick={handleNavigationToContactMePage}> Hire Me</button>
+        </div>
+      </Animate>
     </section>
   );
 };
