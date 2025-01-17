141ac21a6e32d71f192280ab1133152a8f2b4458
Avinash Gupta <avinash5693@gmail.com>
2025-01-18 00:48:36 +0530

    Finished with the about us page.

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -1,85 +1,115 @@
-import React from 'react';
-import './styles.scss';
+import React from "react";
+import "./styles.scss";
 import PageHeaderContent from "../../components/pageHeaderContent/index";
-import { BsInfoCircleFill } from 'react-icons/bs';
-import { Animate } from 'react-simple-animate';
+import { BsInfoCircleFill } from "react-icons/bs";
+import { Animate } from "react-simple-animate";
 
+import { DiApple, DiAndroid } from "react-icons/di";
+import { FaDev, FaDatabase } from "react-icons/fa";
 const personalDetails = [
-    {
-        label: "Name",
-        value: "Avinash Gupta"
-    },
-    {
-        label: "Age",
-        value: "31"
-    },
-    {
-        label: "Address",
-        value: "India"
-    },
-    {
-        label: "Email",
-        value: "avinash5693@gmail.com"
-    },
-    {
-        label: "Contact No",
-        value: "+91 9768145640"
-    }
+  {
+    label: "Name",
+    value: "Avinash Gupta",
+  },
+  {
+    label: "Age",
+    value: "31",
+  },
+  {
+    label: "Address",
+    value: "India",
+  },
+  {
+    label: "Email",
+    value: "avinash5693@gmail.com",
+  },
+  {
+    label: "Contact No",
+    value: "+91 9768145640",
+  },
 ];
 
-const About = () =>  {
+const About = () => {
+  const jobSummary =
+    "Seasoned and independent Front End Developer with 5 years of experience in blending the art of design with skill of programming to deliver an immersive and engaging user experience through efficient website development, proactive feature optimization, and relentless debugging. Very passionate about aesthetics and UI design. It is imperative that you provide a thorough and professional approach to your resume. As a Front End Developer you'll be judged by your ability to use UX and UI concepts and follow design guidelines. It's about expressing your attention to detail and how you can help implement design ideas for your future employer.";
+  return (
+    <section id="about" className="about">
+      <PageHeaderContent
+        headerText="About Me"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="about__content">
+        <div className="about__content__personalWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(-900px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3> Front End Developer </h3>
+            <p>{jobSummary}</p>
+          </Animate>
 
-    const jobSummary = "Seasoned and independent Front End Developer with 5 years of experience in blending the art of design with skill of programming to deliver an immersive and engaging user experience through efficient website development, proactive feature optimization, and relentless debugging. Very passionate about aesthetics and UI design. It is imperative that you provide a thorough and professional approach to your resume. As a Front End Developer you'll be judged by your ability to use UX and UI concepts and follow design guidelines. It's about expressing your attention to detail and how you can help implement design ideas for your future employer."
-    return (
-        <section id="about" className="about">
-            <PageHeaderContent
-            headerText="About Me"
-            icon={<BsInfoCircleFill size={40} />} 
-                />
-                <div className="about__content">
-                    <div className="about__content__personalWrapper">
-                    <Animate
-                          play
-                          duration={1.5}
-                          delay={1}
-                          start={{
-                          transform: "translateX(-900px)"
-                          }}
-                          end={{
-                          transform: "translatex(0px)"
-                          }}>
-                <h3> Front End Developer </h3>
-                <p>{jobSummary}</p>
-                </Animate>
-
-                <Animate
-                          play
-                          duration={1.5}
-                          delay={1}
-                          start={{
-                          transform: "translateX(5  00px)"
-                          }}
-                          end={{
-                          transform: "translatex(0px)"
-                          }}>
-                <h3 className="personalInformationHeaderText">Personal Information</h3>
-                <ul>
-                          {
-                            personalDetails.map((item,i)=> {
-                                <li key={i}>
-                                    <span className="title">{item.label}</span>
-                                    <span className="value">{item.value}</span>
-                                </li>
-                            } )
-                          }
-                </ul>
-                </Animate>
-                    </div>
-                    <div className="about__content__servicesWrapper">Services Wrapper</div>
-                   
-                </div>
-        </section>
-    );
-}
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(500px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3 className="personalInformationHeaderText">
+              Personal Information
+            </h3>
+            <ul>
+              {personalDetails.map((item, i) => (
+                <li key={i}>
+                  <span className="title">{item.label}</span>
+                  <span className="value">{item.value}</span>
+                </li>
+              ))}
+            </ul>
+          </Animate>
+        </div>
+        <div className="about__content__servicesWrapper">
+        <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(600px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+          <div className="about__content__servicesWrapper__innerContent">
+            <div>
+              <FaDev size={60} color="var(--yellow-theme-main-color)" />
+            </div>
+            <div>
+              <DiAndroid size={60} color="var(--yellow-theme-main-color)" />
+            </div>
+            <div>
+              <FaDatabase size={60} color="var(--yellow-theme-main-color)" />
+            </div>
+            <div>
+              <DiApple size={60} color="var(--yellow-theme-main-color)" />
+            </div>
+          </div>
+          </Animate>
+        </div>
+      </div> 
+    </section>
+  );
+};
 
 export default About;
